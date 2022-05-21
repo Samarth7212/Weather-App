051b44c985461cb6e3106c9d102b8d7db56e808e
Samarth7212 <samarthkamble7212@gmail.com>
2022-05-21 23:53:49 +0530

    Printed latitude and longitude with error handling. Added call back function for geocode and forecast.

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
 const geocode = require('./utils/geocode.js');
+const forecast = require('./utils/forecast');
 const request = require('request');
-
-// const url = 'http://api.weatherstack.com/current?access_key=3bdf7ff4f86a9e6ce82f8f40630be981&query=37.8267,-122.4233&unit=s'
-// // const url = 'http://api.weatherstack.com/current?access_key=3bdf7ff4f86a9e6ce82f8f40630be981&query='
-// request({ url: url, json: true }, (error, response) => {
-//     if (error) {
-//         console.log(error)
-//     } else if (response.body.error) {
-//         console.log(response.body.error.info)
-//     } else {
-//         const data = response.body.current
-//         console.log(data.name)
-//         console.log("Latitude: " + data.lat)
-//         console.log("Longitude: " + data.lon)
-//     }
-// })
 
 geocode.geocode('Boston', (error, response) => {
     if (error) console.log('error: ', error)
     else console.log('Data', response)
+})
+
+forecast.forecast(37.8267, -122.4233, (error, response) => {
+    if (error) console.log('error: ', error)
+    else console.log('Data: ', response)
 })
 
---
+++ utils/forecast.js
@@ -0,0 +1,25 @@
+const request = require('request');
+
+const forecast = (lat, lon, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=3bdf7ff4f86a9e6ce82f8f40630be981&query=' + encodeURIComponent(lat) + ',' + encodeURIComponent(lon) + '&unit=s'
+    request({ url: url, json: true }, (error, response) => {
+        if (error) {
+            callback('Error! Unable to connect', undefined)
+        }
+        else if (response.body.error) {
+            callback('No place by this name', undefined)
+        }
+        else {
+            const data = response.body.location
+            callback(undefined, {
+                latitude: data.lat,
+                longitude: data.lon,
+                location: data.name,
+            })
+        }
+    })
+}
+
+module.exports = {
+    forecast,
+}
