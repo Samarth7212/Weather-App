acb2fc8b694a3e8d46641ef9be2ad7c505c175b1
Samarth7212 <samarthkamble7212@gmail.com>
2022-05-22 12:46:45 +0530

    Used object destructuring and shorthand property.

--- utils/forecast.js
+++ utils/forecast.js
@@ -2,21 +2,22 @@ const request = require('request');
 
 const forecast = (lat, lon, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=3bdf7ff4f86a9e6ce82f8f40630be981&query=' + encodeURIComponent(lat) + ',' + encodeURIComponent(lon) + '&unit=s'
-    request({ url: url, json: true }, (error, response) => {
+    request({ url: url, json: true }, (error, { body }) => {
         if (error) {
             callback('Error! Unable to connect', undefined)
         }
-        else if (response.body.error) {
+        else if (body.error) {
             callback('No place by this name', undefined)
         }
         else {
-            const data = response.body.location
+            const { name, lat, lon } = body.location
+            const { temperature, wind_speed } = body.current
             callback(undefined, {
-                latitude: data.lat,
-                longitude: data.lon,
-                location: data.name,
-                temperature: response.body.current.temperature,
-                windSpeed: response.body.current.wind_speed,
+                latitude: lat,
+                longitude: lon,
+                location: name,
+                temperature: temperature,
+                windSpeed: wind_speed,
             })
         }
     })
--- utils/geocode.js
+++ utils/geocode.js
@@ -2,19 +2,20 @@ const request = require('request');
 
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1Ijoic2FtYXJ0aDcyMTIiLCJhIjoiY2wzZmV6dGxwMGEyMTNjbjBxMDd0dDN3eSJ9.OyzLAUEodowsZvXSu81XOw'
-    request({ url: url, json: true }, (error, response) => {
+    request({ url: url, json: true }, (error, { body }) => {
         if (error) {
             callback('Error! Unable to connect', undefined)
-        } else if (response.statusCode != 200) {
-            console.log(response.body.message)
-        } else if (response.body.features.length == 0) {
+        } else if (body.message) {
+            console.log(body.message)
+        } else if (body.features.length == 0) {
             callback('No place by this name', undefined)
         } else {
-            const data = response.body.features[0]
+            const name = body.features[0].place_name
+            const data = body.features[0].center
             callback(undefined, {
-                latitude: data.center[1],
-                longitude: data.center[0],
-                location: data.place_name,
+                latitude: data[1],
+                longitude: data[0],
+                location: name,
             })
         }
     })
